e9fdbb5ecd628bb32c0774cdb707e1e26fbda8b7
Max Kovpak <max_kovpak@hotmail.com>
2015-10-08 00:19:32 +0300

    Update lazy-router.js

--- lazy-router.js
+++ lazy-router.js
@@ -2,33 +2,40 @@ define(function() {
     var Router = function(options) {
         options || (options = {});
         if (options.routes) this.routes = options.routes;
-        
+
         this._bindRoutes();
         this.initialize.apply(this, arguments);
     };
 
     _.extend(Router.prototype, Backbone.Events, {
         initialize: function(){},
-
         routes: {},
         router: null,
 
         _bindRoutes: function() {
             var routes = {};
             _.forEach(this.routes, function(controller, route) {
-                var action = 'index';
-                if(controller.match('@')) {
-                    var arr = controller.split('@');
-                    controller = arr[0];
-                    action = arr[1];
-                }
+                if (typeof controller == 'function') {
+                    routes[route] = controller;
+                } else {
+                    var action = 'index';
+                    if (controller.match('@')) {
+                        var arr = controller.split('@');
+                        controller = arr[0];
+                        action = arr[1];
+                    }
 
-                routes[route] = function() {
-                    require([
-                        controller
-                    ], function(controller) {
-                        controller[action].apply(controller, arguments);
-                    });
+                    routes[route] = function() {
+                        require([
+                            controller
+                        ], function(controller) {
+                            if (typeof controller == 'function') {
+                                controller.apply(controller, arguments);
+                            } else {
+                                controller[action].apply(controller, arguments);
+                            }
+                        });
+                    }
                 }
             }, this);
 
